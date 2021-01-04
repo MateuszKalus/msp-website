e247406e65ac1486de3deed1f9dfc80c555099f9
Mateusz Kalus <mat.kalus@gmail.com>
2021-01-04 12:04:43 +0100

    change breadcrump - next attempt

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,23 @@
 import * as React from "react"
-import { Link, navigate } from "gatsby"
+import {Link, navigate} from "gatsby"
 import MainLayoult from "../templates/main-layoult";
 
 // markup
 const ErrPage = ({location, pageContext}) => {
-    navigate("/");
+
+    if (typeof window !== `undefined`) {
+        navigate("/");
+    }
 
     const {
-        breadcrumb: { crumbs },
+        breadcrumb: {crumbs},
     } = pageContext;
 
     return (
-    <MainLayoult location={location} crumbLabel="Strona nie istnieje" crumbs={crumbs}>
-      404
-    </MainLayoult>
-  )
+        <MainLayoult location={location} crumbLabel="Strona nie istnieje" crumbs={crumbs}>
+            404
+        </MainLayoult>
+    )
 }
 
 export default ErrPage
